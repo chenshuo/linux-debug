0ce9b2e56b38962a5ab82f87960746c8d9d7c3c4
Shuo Chen <chenshuo@chenshuo.com>
2017-01-24 23:48:41 -0800

    skb_init() and tcp_v4_init()

--- build.sh
+++ build.sh
@@ -4,12 +4,13 @@ set -x
 g++ -m32 -g study/main.cc study/lib.cc -Wall -Wl,--gc-sections \
   lib/ctype.o \
   lib/kstrtox.o \
+  net/core/request_sock.o \
+  net/core/skbuff.o \
   net/ipv4/inet_hashtables.o \
   net/ipv4/tcp.o \
   net/ipv4/tcp_cong.o \
   net/ipv4/tcp_input.o \
   net/ipv4/tcp_ipv4.o \
   net/ipv4/tcp_minisocks.o \
-  net/core/request_sock.o \
   study/stub_net.o \
   study/stub_mm.o
--- study/main.cc
+++ study/main.cc
@@ -3,12 +3,27 @@
 extern "C"
 {
 
+// net/socket.c
+struct socket;
+extern int sock_create(int family, int type, int protocol, struct socket **res);
+
+// net/core/skbuff.c
+void skb_init(void);
+
 // net/ipv4/tcp.c
 void tcp_init();
 
+// net/ipv4/tcp_ipv4.c
+// void tcp_v4_init();
+struct inet_hashinfo;
+void inet_hashinfo_init(struct inet_hashinfo *h);
+extern struct inet_hashinfo tcp_hashinfo;
 }
 
 int main()
 {
+  skb_init();
+  // tcp_v4_init();
+  inet_hashinfo_init(&tcp_hashinfo);
   tcp_init();
 }
